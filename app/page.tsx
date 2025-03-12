08dc94d6f2466385e12328c3f5fad8bef5157fda
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-03-12 17:20:01 +0100

    removed image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 import "@ant-design/v5-patch-for-react-19";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 import { Button } from "antd";
 import { BookOutlined, CodeOutlined, GlobalOutlined } from "@ant-design/icons";
 import styles from "@/styles/page.module.css";
@@ -11,14 +10,6 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {/* <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        /> */}
         <h2>SoPra Group 19:</h2>
         <p>
           <strong>Members:</strong> Anabel Nigsch, Benedikt Jung, Elisabeth
