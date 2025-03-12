eefb1da15b618298e592e39bd44719cac5988aa4
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-03-12 16:32:12 +0100

    added server url

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -6,8 +6,9 @@ import { isProduction } from "@/utils/environment";
  * In development, it returns "http://localhost:8080".
  */
 export function getApiDomain(): string {
-  const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
-    "http://localhost:8080"; // TODO: update with your production URL as needed.
+  const prodUrl =
+    process.env.NEXT_PUBLIC_PROD_API_URL ||
+    "https://sopra-fs25-group-19-server.oa.r.appspot.com/";
   const devUrl = "http://localhost:8080";
   return isProduction() ? prodUrl : devUrl;
 }
