78d8cb77ab190503e738d77a57766e57b6ee4384
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-03-27 00:01:44 +0100

    setup project done

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
-import "@ant-design/v5-patch-for-react-19";
-import { useRouter } from "next/navigation";
-
-export default function Home() {
-  const router = useRouter();
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
--- app/types/error.ts
+++
@@ -1,4 +0,0 @@
-export interface ApplicationError extends Error {
-  info: string;
-  status: number;
-}
