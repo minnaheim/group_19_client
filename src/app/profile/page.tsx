c240bb46d6ac31e8b918acb629c5c924eb087fb3
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-03-27 22:04:42 +0100

    added login/register page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,1 @@
+// TODO: Vertical Navigation Bar: with this for example https://ui.shadcn.com/docs/components/sidebar
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -18,94 +18,74 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Projector } from "lucide-react";
 
 // TODO: add mappign that when clicking on the different tabs, you get sent to different login and register subpages
 export default function Register() {
   return (
-    <div>
-      <Card className="w-[400px]">
-        <Tabs defaultValue="account" className="w-[400px]">
-          <TabsList>
-            <TabsTrigger value="registration">Register</TabsTrigger>
-            <TabsTrigger value="login">Login</TabsTrigger>
-          </TabsList>
-          <TabsContent value="registration">
-            <CardContent>
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="name">Name</Label>
-                    <Input id="name" placeholder="Input your Name" />
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="framework">Username</Label>
-                    <Input id="name" placeholder="Input your Username" />
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="framework">Password</Label>
-                    <Input id="name" placeholder="Input your password" />
-                  </div>
-                </div>
-              </form>
-              <br></br>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button variant="destructive">back</Button>
-              <Button>Submit</Button>
-            </CardFooter>
-          </TabsContent>
-          <TabsContent value="login">
-            <CardContent>
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="name">Username</Label>
-                    <Input id="name" placeholder="Input your Username" />
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="framework">Password</Label>
-                    <Input id="name" placeholder="Input your password" />
-                  </div>
-                </div>
-              </form>
-              <br></br>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button variant="destructive">back</Button>
-              <Button>Submit</Button>
-            </CardFooter>
-          </TabsContent>
-        </Tabs>
-      </Card>
+    <div className="h-screen w-full grid grid-cols-10">
+      {/* Left Side (30%) */}
+      {/* <Projector />  TODO: -> add projector here */}
+      <div className="col-span-4 bg-purple-300"></div>
+      {/* Right Side (70%) */}
+      <div className="col-span-6 flex items-center justify-center bg-gray-100">
+        <Card className="w-[400px] p-6">
+          <Tabs defaultValue="login" className="w-full">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="registration">Register</TabsTrigger>
+              <TabsTrigger value="login">Login</TabsTrigger>
+            </TabsList>
+            <TabsContent value="registration">
+              <Card>
+                <CardContent className="space-y-2">
+                  <form>
+                    <div className="grid w-full items-center gap-4">
+                      <div className="flex flex-col space-y-1.5">
+                        <Label htmlFor="name">Name</Label>
+                        <Input id="name" placeholder="Input your Name" />
+                      </div>
+                      <div className="flex flex-col space-y-1.5">
+                        <Label htmlFor="framework">Username</Label>
+                        <Input id="name" placeholder="Input your Username" />
+                      </div>
+                      <div className="flex flex-col space-y-1.5">
+                        <Label htmlFor="framework">Password</Label>
+                        <Input id="name" placeholder="Input your password" />
+                      </div>
+                    </div>
+                  </form>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                  <Button variant="destructive">back</Button>
+                  <Button>Submit</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="login">
+              <Card>
+                <CardContent className="space-y-2">
+                  <form>
+                    <div className="grid w-full items-center gap-4">
+                      <div className="flex flex-col space-y-1.5">
+                        <Label htmlFor="name">Username</Label>
+                        <Input id="name" placeholder="Input your Username" />
+                      </div>
+                      <div className="flex flex-col space-y-1.5">
+                        <Label htmlFor="framework">Password</Label>
+                        <Input id="name" placeholder="Input your password" />
+                      </div>
+                    </div>
+                  </form>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                  <Button variant="destructive">back</Button>
+                  <Button>Submit</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </Card>
+      </div>
     </div>
   );
 }
-
-// export default function Register() {
-//   return (
-//     <Card className="w-[350px]">
-//       <CardHeader>
-//         <CardTitle>Register</CardTitle>
-//         <CardDescription>If you are a new user.</CardDescription>
-//       </CardHeader>
-//       <CardContent>
-// <form>
-//   <div className="grid w-full items-center gap-4">
-//     <div className="flex flex-col space-y-1.5">
-//       <Label htmlFor="name">Username</Label>
-//       <Input id="name" placeholder="Input your Username" />
-//     </div>
-//     <div className="flex flex-col space-y-1.5">
-//       <Label htmlFor="framework">Password</Label>
-//       <Input id="name" placeholder="Input your password" />
-//     </div>
-//   </div>
-// </form>
-//       </CardContent>
-//       <CardFooter className="flex justify-between">
-//         <Button variant="destructive">back</Button>
-//         <Button>Submit</Button>
-//       </CardFooter>
-//     </Card>
-//   );
-// }
