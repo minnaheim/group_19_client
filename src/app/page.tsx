529c6c120d8e28b5c3c4372989239040f346123d
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-03-28 17:12:32 +0100

    added links to buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button variant={"default"}>Submit</Button>
-      <Button variant={"secondary"}>Second Button</Button>
-      {/* change opacity for the default and secondary button and */}
-      <Button variant={"destructive"}>back</Button>
+    // "col-span-6 flex items-center justify-center bg-[#e8f0fe] max-md: h-screen"
+    // "flex h-screen w-full md:grid grid-cols-10">
+    <div className="col-span-6 flex items-center justify-center bg-[#e8f0fe] max-md: h-screen">
+      <Card className="w-[300px] p-6 m-6">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <p>
+          <Link href="/register">
+            <Button variant={"default"}>Go to Register!</Button>
+          </Link>
+        </p>
+        <p>
+          <Link href="/login">
+            <Button variant={"secondary"}>Go to Login!</Button>
+          </Link>
+        </p>
+        <p>
+          {/* change opacity for the default and secondary button and */}
+          <Button variant={"destructive"}>back</Button>
+        </p>
+      </Card>
     </div>
   );
 }
