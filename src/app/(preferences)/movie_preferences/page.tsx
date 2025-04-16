8f9a9863fe7a9440f2d4003db826313962ec6a69
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-04-16 22:13:24 +0200

    added movie changes

--- src/app/(preferences)/movie_preferences/page.tsx
+++ src/app/(preferences)/movie_preferences/page.tsx
@@ -5,7 +5,7 @@ import { Movie } from "@/app/types/movie";
 import MovieListHorizontal from "@/components/ui/movie_list_horizontal";
 import SearchBar from "@/components/ui/search_bar";
 import { Button } from "@/components/ui/button";
-import { useRouter, useParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useApi } from "@/app/hooks/useApi";
 
 const MoviePreferences: React.FC = () => {
@@ -14,169 +14,32 @@ const MoviePreferences: React.FC = () => {
   const [searchCategory, setSearchCategory] = useState<string>("all");
   const [isSearching, setIsSearching] = useState<boolean>(false);
   const [searchResults, setSearchResults] = useState<Movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
   const apiService = useApi();
   const router = useRouter();
-  const { id } = useParams();
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre"); // Extract the genre from query parameters
 
-  // Mock movies for testing
-  const mockMovies: Movie[] = [
-    {
-      movieId: 1,
-      title: "To All the Boys I've Loved Before",
-      posterURL: "/hKHZhUbIyUAjcSrqJThFGYIR6kI.jpg",
-      description:
-        "A teenage girl's secret love letters are exposed and wreak havoc on her love life. To save face, she begins a fake relationship with one of the recipients.",
-      genres: ["Teen Romance"],
-      directors: ["Susan Johnson"],
-      actors: ["Lana Condor", "Noah Centineo", "Janel Parrish"],
-      trailerURL: "https://www.example.com/to-all-the-boys",
-      year: 2002,
-      originallanguage: "English",
-    },
-    {
-      movieId: 2,
-      title: "The Kissing Booth",
-      posterURL: "/7Dktk2ST6aL8h9Oe5rpk903VLhx.jpg",
-      description:
-        "A high school student finds herself face-to-face with her long-term crush when she signs up to run a kissing booth at the spring carnival.",
-      genres: ["Teen Romance"],
-      directors: ["Vince Marcello"],
-      actors: ["Joey King", "Jacob Elordi", "Joel Courtney"],
-      trailerURL: "https://www.example.com/kissing-booth",
-      year: 2018,
-      originallanguage: "English",
-    },
-    {
-      movieId: 3,
-      title: "Dune: Part Two",
-      posterURL: "/8b8R8l88Qje9dn9OE8PY05Nxl1X.jpg",
-      description:
-        "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.",
-      genres: ["Science Fiction"],
-      directors: ["Denis Villeneuve"],
-      actors: ["Timothée Chalamet", "Zendaya", "Rebecca Ferguson"],
-      trailerURL: "https://www.example.com/dune-part-two",
-      year: 2023,
-      originallanguage: "English",
-    },
-    {
-      movieId: 4,
-      title: "Oppenheimer",
-      posterURL: "/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
-      description:
-        "The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb.",
-      genres: ["Drama"],
-      directors: ["Christopher Nolan"],
-      actors: ["Cillian Murphy", "Emily Blunt", "Matt Damon"],
-      trailerURL: "https://www.example.com/oppenheimer",
-      year: 2023,
-      originallanguage: "English",
-    },
-    {
-      movieId: 5,
-      title: "Poor Things",
-      posterURL: "/kCGlIMHnOm8JPXq3rXM6c5wMxcT.jpg",
-      description:
-        "The incredible tale about the fantastical evolution of Bella Baxter, a young woman brought back to life by the brilliant and unorthodox scientist Dr. Godwin Baxter.",
-      genres: ["Science Fiction"],
-      directors: ["Yorgos Lanthimos"],
-      actors: ["Emma Stone", "Mark Ruffalo", "Willem Dafoe"],
-      trailerURL: "https://www.example.com/poor-things",
-      year: 2023,
-      originallanguage: "English",
-    },
-    {
-      movieId: 11,
-      title: "Oppenheimer",
-      posterURL: "/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
-      description:
-        "The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb.",
-      genres: ["Drama"],
-      directors: ["Christopher Nolan"],
-      actors: ["Cillian Murphy", "Emily Blunt", "Matt Damon"],
-      trailerURL: "https://www.example.com/oppenheimer",
-      year: 2023,
-      originallanguage: "English",
-    },
-    {
-      movieId: 12,
-      title: "Poor Things",
-      posterURL: "/kCGlIMHnOm8JPXq3rXM6c5wMxcT.jpg",
-      description:
-        "The incredible tale about the fantastical evolution of Bella Baxter, a young woman brought back to life by the brilliant and unorthodox scientist Dr. Godwin Baxter.",
-      genres: ["Science Fiction"],
-      directors: ["Yorgos Lanthimos"],
-      actors: ["Emma Stone", "Mark Ruffalo", "Willem Dafoe"],
-      trailerURL: "https://www.example.com/poor-things",
-      year: 2023,
-      originallanguage: "English",
-    },
-    {
-      movieId: 6,
-      title: "The Fall Guy",
-      posterURL: "/6OnoMgGFuZ921eV8v8yEyXoag19.jpg",
-      description:
-        "A stuntman is drawn back into service when the star of a mega-budget studio movie goes missing.",
-      genres: ["Action"],
-      directors: ["David Leitch"],
-      actors: ["Ryan Gosling", "Emily Blunt", "Aaron Taylor-Johnson"],
-      trailerURL: "https://www.example.com/fall-guy",
-      year: 2024,
-      originallanguage: "English",
-    },
-    {
-      movieId: 7,
-      title: "The Batman",
-      posterURL: "/74xTEgt7R36Fpooo50r9T25onhq.jpg",
-      description:
-        "When a sadistic serial killer begins murdering key political figures in Gotham, Batman is forced to investigate the city's hidden corruption and question his family's involvement.",
-      genres: ["Action"],
-      directors: ["Matt Reeves"],
-      actors: ["Robert Pattinson", "Zoë Kravitz", "Paul Dano"],
-      trailerURL: "https://www.example.com/the-batman",
-      year: 2022,
-      originallanguage: "English",
-    },
-    {
-      movieId: 8,
-      title: "The Whale",
-      posterURL: "/jQ0gylJMxWSL490sy0RrPj1Lj7e.jpg",
-      description:
-        "A reclusive English teacher attempts to reconnect with his estranged teenage daughter.",
-      genres: ["Drama"],
-      directors: ["Darren Aronofsky"],
-      actors: ["Brendan Fraser", "Sadie Sink", "Hong Chau"],
-      trailerURL: "https://www.example.com/the-whale",
-      year: 2022,
-      originallanguage: "English",
-    },
-    {
-      movieId: 9,
-      title: "Top Gun: Maverick",
-      posterURL: "/62HCnUTziyWcpDaBO2i1DX17ljH.jpg",
-      description:
-        "After more than thirty years of service as one of the Navy's top aviators, Pete Mitchell is where he belongs, pushing the envelope as a courageous test pilot and dodging the advancement in rank that would ground him.",
-      genres: ["Action"],
-      directors: ["Joseph Kosinski"],
-      actors: ["Tom Cruise", "Miles Teller", "Jennifer Connelly"],
-      trailerURL: "https://www.example.com/top-gun-maverick",
-      year: 2022,
-      originallanguage: "English",
-    },
-    {
-      movieId: 10,
-      title: "Everything Everywhere All at Once",
-      posterURL: "/w3LxiVYdWWRvEVdn5RYq6jIqkb1.jpg",
-      description:
-        "An aging Chinese immigrant is swept up in an insane adventure, where she alone can save the world by exploring other universes connecting with the lives she could have led.",
-      genres: ["Science Fiction"],
-      directors: ["Daniel Kwan", "Daniel Scheinert"],
-      actors: ["Michelle Yeoh", "Ke Huy Quan", "Jamie Lee Curtis"],
-      trailerURL: "https://www.example.com/everything-everywhere",
-      year: 2022,
-      originallanguage: "English",
-    },
-  ];
+  // Fetch movies based on the selected genre from the backend
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await apiService.get<Movie[]>(
+          `/movies?genreList=${genre}`
+        );
+        setMovies(response); // Store the movies in state
+      } catch (error) {
+        console.error("Failed to fetch movies based on genre:", error);
+        alert(
+          "An error occurred while fetching movies based on genre. Please try again."
+        );
+      }
+    };
+
+    if (genre) {
+      fetchMovies();
+    }
+  }, [apiService, genre]);
 
   // Search logic
   useEffect(() => {
@@ -189,13 +52,13 @@ const MoviePreferences: React.FC = () => {
     setIsSearching(true);
 
     const query = searchQuery.toLowerCase().trim();
-    const filtered = mockMovies.filter((movie) => {
+    const filtered = movies.filter((movie) => {
       if (searchCategory === "title" || searchCategory === "all") {
         if (movie.title.toLowerCase().includes(query)) return true;
       }
 
       if (searchCategory === "genre" || searchCategory === "all") {
-        if (movie.genres.some((genre) => genre.toLowerCase().includes(query))) {
+        if (movie.genres.some((g) => g.toLowerCase().includes(query))) {
           return true;
         }
       }
@@ -204,7 +67,7 @@ const MoviePreferences: React.FC = () => {
     });
 
     setSearchResults(filtered);
-  }, [searchQuery, searchCategory]);
+  }, [searchQuery, searchCategory, movies]);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
@@ -223,7 +86,6 @@ const MoviePreferences: React.FC = () => {
 
   const toggleMovie = (movie: Movie) => {
     setSelectedMovies((prev) => {
-      console.log(prev);
       if (prev.some((m) => m.movieId === movie.movieId)) {
         return prev.filter((m) => m.movieId !== movie.movieId);
       } else {
@@ -231,7 +93,6 @@ const MoviePreferences: React.FC = () => {
           alert("You can only select one favorite movie");
           return prev;
         }
-        console.log([...prev, movie]);
         return [...prev, movie];
       }
     });
@@ -260,7 +121,7 @@ const MoviePreferences: React.FC = () => {
     }
   };
 
-  const displayMovies = isSearching ? searchResults : mockMovies;
+  const displayMovies = isSearching ? searchResults : movies;
 
   return (
     <div>
