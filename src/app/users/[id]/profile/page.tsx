a6875fb861329683e39e1c1dd9650dc225c62c85
ellaruby0 <ella@philippi-net.com>
2025-04-25 08:25:44 +0200

    second try

--- src/app/users/[id]/profile/page.tsx
+++ src/app/users/[id]/profile/page.tsx
@@ -64,7 +64,7 @@ const Profile: React.FC = () => {
   };
 
   const handleBack = () => {
-    router.push(`users/${id}/dashboard`);
+    router.push(`/users/${id}/dashboard`);
   };
 
   const showMessage = (message: string) => {
