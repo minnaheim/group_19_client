621b814f53bcb73cd60d2b1ea3b2b7cd569036e4
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-05-18 11:27:11 +0200

    trying to fix mobile zoom in issue from nils

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+  themeColor: "black",
+};
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
